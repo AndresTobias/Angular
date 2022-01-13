d93d2c644dcea81ab13bcc4fa8665f8d1f8b553c
andres74ar@gmail.com <andres74ar@gmail.com>
2022-01-13 14:33:05 -0300

    primer commit para agregar a github el tutorial de angular

---
+++ src/app/products.ts
@@ -0,0 +1,40 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    id: 3,
+    name: 'Phone Standard',
+    price: 299,
+    description: 'no me gusto que no tenga descripcion'
+  },
+  {
+    id: 4,
+    name: 'Phone legendario',
+    price: 999.99,
+    description: 'Nokia 1100 La Maquina'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
